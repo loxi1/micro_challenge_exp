cbc238c08f6464c8792535af6b4865b6868c4122
loxi1 <anibal.cayetano@gmail.com>
2023-01-26 15:15:58 -0500

    26-01-2023 colocar la documentación con swagger en la pagina de inicio

--- routes/FechaRoter.js
+++ routes/FechaRoter.js
@@ -2,10 +2,21 @@ const express = require("express");
 const datetime = require('node-datetime');
 const FechaRoter = express.Router();
 let dt = datetime.create();
+const estilo = `position: relative;
+background-color: #4CAF50;
+border: none;
+font-size: 30px;
+color: #FFFFFF;
+padding: 25px;
+width: 300px;
+text-align: center;
+transition-duration: 0.4s;
+text-decoration: none;
+overflow: hidden;
+cursor: pointer;`
 
-let {_now} = dt;
 FechaRoter.get("/", async (req, res) => {
-    res.send('Bienvenido al Api')
+    res.send("<a href='/docs' style='text-decoration:none'><button style='"+`${estilo}`+"'><img src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Swagger-logo.png'> Click Swagger UI</button></a>")
 })
 
 FechaRoter.get("/what-time", async (req, res) => {
@@ -13,7 +24,7 @@ FechaRoter.get("/what-time", async (req, res) => {
         return res.status(200).send({
             success: true,
             message: "Ok",
-            response: dt.format('H:M:S')
+            response: dt.format('I:M:S p')
         })
     } catch (error) {
         return res.status(500).send({
@@ -28,7 +39,7 @@ FechaRoter.get("/what-date", async (req, res) => {
         return res.status(200).send({
             success: true,
             message: "Ok",
-            response: dt.format('Y-m-S')
+            response: dt.format('Y-m-d')
         })
     } catch (error) {
         return res.status(500).send({
@@ -43,7 +54,7 @@ FechaRoter.get("/what-date-time", async (req, res) => {
         return res.status(200).send({
             success: true,
             message: "Ok",
-            response: _now
+            response: dt.format('Y-m-d I:M:S p')
         })
     } catch (error) {
         return res.status(500).send({
