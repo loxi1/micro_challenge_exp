96eef9d82e688161db8a6ec0df3fabfa42a0cf56
loxi1 <anibal.cayetano@gmail.com>
2023-02-01 21:11:10 -0500

    cmabio en el puerto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const FechaRoter = require('./routes/FechaRoter');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
+const PORT = process.env.PORT || 3000
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded());
 
@@ -12,6 +12,6 @@ app.use("/", FechaRoter);
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Servidor escucha el puerto 3000")
 });
